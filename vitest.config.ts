6cc83fe36a1b3c22719e08b3a59e2b080bd0ed31
michelle enos <32843306+crankysparrow@users.noreply.github.com>
2026-05-12 21:20:07 -0400

    stop vitest from doing coverage by default

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
     test: {
         environment: 'node',
         coverage: {
-            enabled: true,
             provider: 'v8',
             include: ['./src/**/*.ts'],
         },
